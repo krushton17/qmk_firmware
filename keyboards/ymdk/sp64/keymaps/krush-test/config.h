06c91f8a34bc0df6539a37c4b863fe4af417ed7d
Kevin Rushton <krushton.sea@gmail.com>
2022-08-18 01:08:21 -0400

    layers from configurator; trying to define hsv's

--- keyboards/ymdk/sp64/keymaps/krush-test/config.h
+++ keyboards/ymdk/sp64/keymaps/krush-test/config.h
@@ -1,3 +1,55 @@
 #pragma once
 
 #define RGBLIGHT_LAYERS
+
+
+#define HSV_KR_TEAL         70, 180, 167
+#define HSV_KR_ORANGE       13, 255, 167
+#define HSV_KR_COOL_BLUE    140, 100, 167
+#define HSV_KR_MAUVE        236, 90, 165
+
+
+// #define HSV_KR_TEAL        
+// 72, 234, 167     // too green
+// 110, 234, 167    // not green enough
+// 90, 234, 167     // either too green or too saturated...
+// 90, 100, 167     // either not green enough or not saturated enough lmao
+// 80, 120, 167     // not bad
+// 75, 130, 167     // nice and icy, getting close!
+// 70, 140, 167     // good. could be even moar green/saturated
+// 70, 160, 167     // even better
+// 70, 200, 167     // definitely too green now
+// 70, 180, 167} // this is pretty good! compare to the "nice and icy" one (this one is better!)
+
+
+// #define HSV_KR_ORANGE       
+// {0, 12, 13, 255, 128} // too dim
+// {0, 12, 13, 255, 167} // pretty good
+
+
+// #define HSV_KR_COOL_BLUE    
+// {0, 12, 128, 240, 183} // too saturated
+// {0, 12, 128, 167, 183} // nees to be bluer
+// {0, 12, 160, 167, 183} // too purple
+// {0, 12, 140, 167, 183} // moar
+// {0, 12, 150, 150, 183} // less
+// {0, 12, 145, 140, 183} // still less. saturation and/or purple
+// {0, 12, 140, 130, 183} // i feel like i want it to be darker somehow? maybe more saturation
+// {0, 12, 130, 160, 183} // at this saturation, it would need more purple
+// {0, 12, 140, 160, 183} // ehh i think just less saturation
+// {0, 12, 145, 130, 183} // more blue, less saturation
+// {0, 12, 140, 130, 167} // less sat
+// {0, 12, 140, 100, 167} //winner!
+// {0, 12, 140, 80, 145} // nope
+// {0, 12, 135, 90, 145} // nope
+
+
+// #define HSV_KR_MAUVE        
+// {0, 12, 236, 207, 165} // too saturated
+// {0, 12, 236, 100, 165} // maybe still slightly to saturated
+// {0, 12, 236, 70, 165} // maybe a little too unsaturated now??
+// {0, 12, 236, 80, 165} //     maybe still slightly too unsaturated
+// {0, 12, 236, 90, 165} // looks good!
+
+
+
--- keyboards/ymdk/sp64/keymaps/krush-test/keymap.c
+++ keyboards/ymdk/sp64/keymaps/krush-test/keymap.c
@@ -9,6 +9,10 @@
 #define _LAYER5 5
 #define _LAYER6 6
 #define _LAYER7 7
+#define _LAYER8 8
+#define _LAYER9 9
+#define _LAYER10 10
+#define _LAYER11 11
 
 enum custom_keycodes {
     LAYER0 = SAFE_RANGE,
@@ -19,13 +23,17 @@ enum custom_keycodes {
     LAYER5,
     LAYER6,
     LAYER7,
+    LAYER8,
+    LAYER9,
+    LAYER10,
+    LAYER11,
 };
 
  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LAYER0] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL, KC_LCTL, KC_LGUI, KC_LALT, MO(2), MO(1), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-[_LAYER1] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, DF(6), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(3), KC_TRNS, TO(4), KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END),
+[_LAYER1] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, DF(6), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(4), KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END),
 
 [_LAYER2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_TRNS, TO(3), KC_MPLY, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_MNXT),
 
@@ -35,9 +43,17 @@ enum custom_keycodes {
 
 [_LAYER5] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_TAB, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, KC_HASH, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_EQL, KC_PERC, KC_P4, KC_P5, KC_P6, KC_COMM, KC_NO, KC_ENT, KC_LPRN, KC_PLUS, KC_PMNS, KC_PAST, KC_PSLS, KC_CIRC, KC_DLR, KC_P1, KC_P2, KC_P3, KC_DOT, KC_RPRN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, TO(0), KC_P0, KC_PDOT, KC_PENT, KC_NO, KC_NO, KC_NO),
 
-[_LAYER6] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL, KC_LCTL, KC_LGUI, KC_LALT, MO(2), MO(7), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+[_LAYER6] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL, KC_LCTL, KC_LGUI, KC_LALT, MO(8), KC_TRNS, KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-[_LAYER7] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, DF(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LGUI, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_TRNS, KC_TRNS, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), KC_RGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, LCTL(KC_DEL), KC_TRNS, KC_TRNS, KC_TRNS, TG(3), KC_TRNS, LCTL(KC_BSPC), KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END) 
+[_LAYER7] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, TO(10), DF(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DYN_REC_START1, DYN_REC_STOP, DYN_MACRO_PLAY1, KC_TRNS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_TRNS, KC_TRNS, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, LCTL(KC_DEL), KC_TRNS, KC_TRNS, KC_TRNS, TG(9), KC_TRNS, LCTL(KC_BSPC), KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END),
+
+[_LAYER8] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_TRNS, TO(9), KC_MPLY, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_MNXT),
+
+[_LAYER9] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_RMOD, RGB_MOD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, RGB_TOG, KC_NO, KC_NO, RGB_HUD, RGB_SAD, RGB_VAD),
+
+[_LAYER10] = LAYOUT(KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_TAB, KC_PLUS, KC_PMNS, KC_PAST, KC_PSLS, KC_CIRC, KC_COMM, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_PERC, KC_DLR, KC_EQL, KC_CAPS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PDOT, KC_ENT, KC_LSFT, KC_BSLS, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT, KC_GT, KC_RPRN, KC_RBRC, KC_RCBR, KC_SLSH, KC_RSFT, KC_UP, KC_DEL, KC_LCTL, KC_LGUI, KC_LALT, TG(11), TO(6), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+
+[_LAYER11] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_NO, KC_BSPC, KC_TAB, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, KC_HASH, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_EQL, KC_PERC, KC_P4, KC_P5, KC_P6, KC_COMM, KC_NO, KC_ENT, KC_LPRN, KC_PLUS, KC_PMNS, KC_PAST, KC_PSLS, KC_CIRC, KC_DLR, KC_P1, KC_P2, KC_P3, KC_DOT, KC_RPRN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_P0, KC_PDOT, KC_PENT, KC_NO, KC_NO, KC_NO) 
 
 };
 
@@ -46,46 +62,23 @@ enum custom_keycodes {
 
 // Light LEDs in cyan when keyboard layer 0 is active
 const rgblight_segment_t PROGMEM rgb_layer_0[] = RGBLIGHT_LAYER_SEGMENTS(
-    // {0, 12, 72, 234, 167} // too green
-    // {0, 12, 110, 234, 167} // not green enough
-    // {0, 12, 90, 234, 167} // either too green or too saturated...
-    // {0, 12, 90, 100, 167} // either not green enough or not saturated enough lmao
-    // {0, 12, 80, 120, 167} // not bad
-    // {0, 12, 75, 130, 167} // nice and icy, getting close!
-    // {0, 12, 70, 140, 167} // good. could be even moar green/saturated
-    // {0, 12, 70, 160, 167} // even better
-    // {0, 12, 70, 200, 167} // definitely too green now
-    {0, 12, 70, 180, 167} // this is pretty good! compare to the "nice and icy" one (this one is better!)
+    // {0, 12, 70, 180, 167}
+    {0, 12, HSV_KR_TEAL}    // defined in config.h. will this even work?
 );
 // Light LEDs in orange when keyboard layer 1 is active
 const rgblight_segment_t PROGMEM rgb_layer_1[] = RGBLIGHT_LAYER_SEGMENTS(
-    // {0, 12, 13, 255, 128} // too dim
-    {0, 12, 13, 255, 167} // pretty good
+    // {0, 12, 13, 255, 167}
+    {0, 12, HSV_KR_ORANGE}
 );
 // Light LEDs in cool blue when keyboard layer 2 is active
 const rgblight_segment_t PROGMEM rgb_layer_2[] = RGBLIGHT_LAYER_SEGMENTS(
-    // {0, 12, 128, 240, 183} // too saturated
-    // {0, 12, 128, 167, 183} // nees to be bluer
-    // {0, 12, 160, 167, 183} // too purple
-    // {0, 12, 140, 167, 183} // moar
-    // {0, 12, 150, 150, 183} // less
-    // {0, 12, 145, 140, 183} // still less. saturation and/or purple
-    // {0, 12, 140, 130, 183} // i feel like i want it to be darker somehow? maybe more saturation
-    // {0, 12, 130, 160, 183} // at this saturation, it would need more purple
-    // {0, 12, 140, 160, 183} // ehh i think just less saturation
-    // {0, 12, 145, 130, 183} // more blue, less saturation
-    // {0, 12, 140, 130, 167} // less sat
-    {0, 12, 140, 100, 167} 
-    // {0, 12, 140, 80, 145}
-    // {0, 12, 135, 90, 145}
+    // {0, 12, 140, 100, 167}
+    {0, 12, HSV_KR_COOL_BLUE}
 );
 // mauve
 const rgblight_segment_t PROGMEM rgb_layer_3[] = RGBLIGHT_LAYER_SEGMENTS(
-    // {0, 12, 236, 207, 165} // too saturated
-    // {0, 12, 236, 100, 165} // maybe still slightly to saturated
-    // {0, 12, 236, 70, 165} // maybe a little too unsaturated now??
-    // {0, 12, 236, 80, 165} //     maybe still slightly too unsaturated
-    {0, 12, 236, 90, 165} // looks good!
+    // {0, 12, 236, 90, 165}
+    {0, 12, HSV_KR_MAUVE}
 );
 // white
 const rgblight_segment_t PROGMEM rgb_layer_4[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -95,25 +88,48 @@ const rgblight_segment_t PROGMEM rgb_layer_4[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t PROGMEM rgb_layer_5[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 12, HSV_RED}
 );
-// mauve
+// why doesn't this one work?? maybe eeprom?
 const rgblight_segment_t PROGMEM rgb_layer_6[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 12, 236, 100, 165}
 );
-// orange
+// Light LEDs in orange when keyboard layer 1 is active
 const rgblight_segment_t PROGMEM rgb_layer_7[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, 13, 255, 167}
+    // {0, 12, 13, 255, 167}
+    {0, 12, HSV_KR_ORANGE}
+);
+// same as layer 2
+const rgblight_segment_t PROGMEM rgb_layer_8[] = RGBLIGHT_LAYER_SEGMENTS(
+    // {0, 12, 140, 100, 167}
+    {0, 12, HSV_KR_COOL_BLUE}
+);
+// same as layer 3
+const rgblight_segment_t PROGMEM rgb_layer_9[] = RGBLIGHT_LAYER_SEGMENTS(
+    // {0, 12, 236, 90, 165}
+    {0, 12, HSV_KR_MAUVE}
+);
+// same as layer 4
+const rgblight_segment_t PROGMEM rgb_layer_10[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 12, 255, 255, 165}
+);
+// same as layer 5
+const rgblight_segment_t PROGMEM rgb_layer_11[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 12, HSV_RED}
 );
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     rgb_layer_0,    // Overrides caps lock layer
     rgb_layer_1,    // Overrides other layers
-    rgb_layer_2,     // Overrides other layers
-    rgb_layer_3,     // Overrides other layers
-    rgb_layer_4,     // Overrides other layers
-    rgb_layer_5,     // Overrides other layers
-    rgb_layer_6,     // Overrides other layers
-    rgb_layer_7
+    rgb_layer_2,
+    rgb_layer_3,
+    rgb_layer_4,
+    rgb_layer_5,
+    rgb_layer_6,
+    rgb_layer_7,
+    rgb_layer_8,
+    rgb_layer_9,
+    rgb_layer_10,
+    rgb_layer_11
     // my_capslock_layer,
 );
 
@@ -129,6 +145,8 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
     return state;
 }
 
+
+// ? for these, can we use the same rgb layer for multiple keyboard layers? like if 7 is like 1, can we just use rgb layer 1 for keymap layer 7?
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, _LAYER1));
     rgblight_set_layer_state(2, layer_state_cmp(state, _LAYER2));
@@ -137,5 +155,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(5, layer_state_cmp(state, _LAYER5));
     rgblight_set_layer_state(6, layer_state_cmp(state, _LAYER6));
     rgblight_set_layer_state(7, layer_state_cmp(state, _LAYER7));
+    rgblight_set_layer_state(8, layer_state_cmp(state, _LAYER8));
+    rgblight_set_layer_state(9, layer_state_cmp(state, _LAYER9));
+    rgblight_set_layer_state(10, layer_state_cmp(state, _LAYER10));
+    rgblight_set_layer_state(11, layer_state_cmp(state, _LAYER11));
     return state;
 }
